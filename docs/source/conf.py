aa90922dede1bab06bb8663db57a297efd2e5675
Adam Tyson <code@adamltyson.com>
2023-10-17 10:55:30 +0100

    Add links to zulip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -113,6 +113,16 @@ html_theme_options = {
             "icon": "fa-brands fa-github",
             # The type of image to be used (see below for details)
             "type": "fontawesome",
+        },
+        {
+            # Label for this link
+            "name": "Zulip (chat)",
+            # URL where the link will redirect
+            "url": "https://neuroinformatics.zulipchat.com/",  # required
+            # Icon class (if "type": "fontawesome"), or path to local image (if "type": "local")
+            "icon": "fa-solid fa-comments",
+            # The type of image to be used (see below for details)
+            "type": "fontawesome",
         }
    ],
    "logo": {
